db5118de8807e3ef3178603c8af7d605aca9d22f
Navid <mojtaba.farokhi99@gmail.com>
2019-06-27 13:46:52 +0430

    initial commit

---
+++ ALU.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:51:57 06/27/2019 
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALU(
+
+    );
+
+endmodule
