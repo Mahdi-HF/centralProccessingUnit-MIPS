8e96fb53a6b9195db931c7047bbbdd50916cea99
mahdi hosseiny farzad <mhf1377@gmail.com>
2019-06-27 17:15:24 +0430

    add SignExtend

--- SignExtend.v
+++ SignExtend.v
@@ -18,8 +18,9 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module SignExtend(
-    );
+module SignExtend(input [15:0]in,
+    output reg [31:0]out );
 
+    assign out = { {16{in[15]} }, in[15:0] };
 
 endmodule
