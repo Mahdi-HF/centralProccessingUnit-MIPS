c7a8310cdeec8fb66402ec6d016a0e1fb0cd7a9a
mahdi hosseiny farzad <mhf1377@gmail.com>
2019-06-27 17:22:24 +0430

    add Shifter2

--- Shifter2.v
+++ Shifter2.v
@@ -18,8 +18,12 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Shifter2(
-    );
+module Shifter2(input [31:0]in,
+    output reg [31:0]out);
 
+    always @*
+    begin
+      in = in<<2;
+    end
 
 endmodule
