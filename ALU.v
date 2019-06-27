d09b5dd08e17732071df5e554b70c10fa1197d6e
Navid <mojtaba.farokhi99@gmail.com>
2019-06-27 15:16:46 +0430

    add ALU Module

--- ALU.v
+++ ALU.v
@@ -19,7 +19,27 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ALU(
-
+    input a[4:0], b[4:0], control[1:0],
+    output reg result[9:0], reg zFlag;
     );
+
+    always @(a,b,control) begin
+        result = 10'b0;
+        zFlag = 1'b0;
+        case (control)
+            2'b00 : result = a & b;
+            2'b01 : result = a ^ b;
+            2'b10 : result = a + b;
+            2'b11 : result = a - b;
+            default: result = 10'bz;
+        endcase
+    end
+
+    always @(a,b,control)
+    begin
+        if (a==b) begin
+            zFlag = 1'b1;
+        end
+    end
 
 endmodule
