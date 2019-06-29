58a1ebeeb48983928fec03502841adbd0d8e54f4
mahdi hosseiny farzad <mhf1377@gmail.com>
2019-06-29 23:26:07 +0430

    all names become the best way and some forgotten wires added in DataPath

--- DataPath.v
+++ DataPath.v
@@ -19,89 +19,90 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module DataPath(input [1:0] aluControl, aluSrcB,
-    input PCSource, aluSrcA, RegWrite, RegDst, isInterrupted, clk,
-    input isBranch, PCWrite, lorD, MemWrite, MemtoReg, IRWrite,
+    input pcSource, aluSrcA, regWrite, regDst, isInterrupted, clk,
+    input isBranch, pcWrite, lorD, memWrite, memToReg, IrWrite,
     output reg [5:0] op, funct);
 
     wire zeroFlag;
     wire pcEnable;
     wire [31:0]Adr;
     wire [31:0]pcIn;
-    wire [31:0]Data;
+    wire [31:0]data;
     wire [31:0]srcA;
     wire [31:0]srcB;
     wire [4:0]dstAdr;
-    wire [31:0]Instr;
+    wire [31:0]instr;
     wire [31:0]aData;
     wire [31:0]bData;
+    wire [31:0]aData2;
     wire [27:0]pcJump;
     wire [31:0]pcOut2;
     wire [31:0]aluOut;
     wire [31:0]memData;
     wire [31:0]signImm;
+    wire [31:0]pcOut100;
     wire [31:0]aluResult;
     wire [31:0]writeData;
-    wire [31:0]writeRegData;
-    reg  [31:0]pcOut = 32'd128;
-	wire [31:0]pcOut100;
     wire [31:0]zero = 32'h0;
+    wire [31:0]writeRegData;
+    wire [31:0]shiftedSignImm;
+    reg  [31:0]pcOut = 32'd128;
     wire [31:0]four = 32'h00000004;
     wire [31:0]interruptAddress = 32'hFFFFFFFF;
 
-	
-	always @(aluControl, aluSrcB, PCSource, aluSrcA, RegWrite, RegDst, isInterrupted, clk, isBranch, PCWrite, lorD, MemWrite, MemtoReg, IRWrite)
+
+	always @(aluControl, aluSrcB, pcSource, aluSrcA, regWrite, regDst,
+    isInterrupted, clk, isBranch, pcWrite, lorD, memWrite, memToReg, IrWrite)
     begin
-		op = Instr[31:26];
-        funct = Instr[5:0];
+		op = instr[31:26];
+        funct = instr[5:0];
     end
 
-    assign pcEnable = (isBranch & zeroFlag) | PCWrite;
+    assign pcEnable = (isBranch & zeroFlag) | pcWrite;
 
     //ShiftRegisterEnable pcReg(pcIn, clk, pcEnable, pcOut);
     always @(posedge clk)  
     begin 
-        if(pcEnable==1'b1) 
-            pcOut <= pcIn; 
+        if(pcEnable==1'b1)
+        begin
+          pcOut <= pcIn;
+        end
     end
-	
-	// initial
-	// 	pcOut100 = pcOut;
 
     Mux2 interruptMux(isInterrupted, pcOut, interruptAddress, pcOut2);
     Mux2 memoryAddressMux(lorD, pcOut2, aluOut, Adr);
 
-    Memory InstrDataMemory(Adr, writeData, clk, MemWrite, memData);
+    Memory instrDataMemory(Adr, writeData, clk, memWrite, memData);
 
-    ShiftRegisterEnable instructionReg(memData, clk, IRWrite, Instr);
+    ShiftRegisterEnable instructionReg(memData, clk, IrWrite, instr);
 	//always @(posedge clk)  
     //begin 
-     //   if(IRWrite==1'b1 && memData != 32'bZ) 
-       //     Instr <= memData; 
+     //   if(IrWrite==1'b1 && memData != 32'bZ) 
+       //     instr <= memData; 
     //end
 	
-	
-    ShiftRegister dataReg(memData, clk, Data);
+    ShiftRegister dataReg(memData, clk, data);
 
-    Mux2FiveBit dstMux(RegDst, Instr[20:16], Instr[15:11], dstAdr);
-    Mux2 writeDataMux(MemtoReg, aluOut, Data, writeRegData);
+    Mux2FiveBit dstMux(regDst, instr[20:16], instr[15:11], dstAdr);
+    Mux2 writeDataMux(memToReg, aluOut, data, writeRegData);
 
-    SignExtend signExtender(Instr[15:0], signImm);
-    Shifter2 shifter(signImm, shiftedsignImm);
+    SignExtend signExtender(instr[15:0], signImm);
+    Shifter2 shifter(signImm, shiftedSignImm);
 
-    RegFile regFile(Instr[25:21], Instr[20:16], dstAdr, writeRegData, RegWrite, clk, aData, bData);
+    RegFile regFile(instr[25:21], instr[20:16], dstAdr, writeRegData, regWrite, clk, aData, bData);
 
     ShiftRegister aDataReg(aData, clk, aData2);
     ShiftRegister bDataReg(bData, clk, writeData);
 
     Mux2 aluPcMux(aluSrcA, pcOut2, aData2, srcA);
 
-    Mux4 srcBMux(aluSrcB, writeData, four, signImm, shiftedsignImm, srcB);
+    Mux4 srcBMux(aluSrcB, writeData, four, signImm, shiftedSignImm, srcB);
 
     ALU mainAlu(srcA, srcB, aluControl, aluResult, zeroFlag);
 
     ShiftRegister aluOutReg(aluResult, clk, aluOut);
 
-    Shifte2New addrShifter(Instr[25:0], pcJump);
-    Mux4 pcBrachMux(PCSource, aluResult, aluOut, {pcOut2[31:28],pcJump}, zero, pcIn);
+    Shifte2New addrShifter(instr[25:0], pcJump);
+    Mux4 pcBrachMux(pcSource, aluResult, aluOut, {pcOut2[31:28],pcJump}, zero, pcIn);
 
 endmodule
--- MultiCycleProccessor.v
+++ MultiCycleProccessor.v
@@ -22,7 +22,7 @@ module MultiCycleProccessor(input clk, INT, NMI, INTD,
     output INA);
     
     wire [5:0]Op, funct;
-    wire [1:0]aluControl2, aluSrcB2;
+    wire [1:0]aluControl, aluSrcB;
     wire PCSource, aluSrc, RegWrite, RegDst, isInterrupted;
     wire isBranch, PCWrite, lorD, MemWrite, MemtoReg, IRWrite;
 
@@ -34,40 +34,3 @@ module MultiCycleProccessor(input clk, INT, NMI, INTD,
     isInterrupted, clk, isBranch, PCWrite, lorD, MemWrite, MemtoReg, IRWrite, Op, funct);
 
 endmodule
- 
-
-
-
-
-
-
-
-
-
-
-
-    // wire PCSource2, ALUSrcA2, RegWrite2, RegDst2, isInterrupted2;
-    // wire isBranch2, PCWrite2, lorD2, MemWrite2, MemtoReg2, IRWrite2;
-
-
-    // assign aluControl2 = aluControl;
-    // assign ALUSrcB2 = ALUSrcB;
-    // assign PCSource2 = PCSource;
-    // assign ALUSrcA2 = ALUSrcA;
-    // assign RegWrite2 = RegWrite;
-    // assign RegDst2 = RegDst;
-    // assign isInterrupted2 = isInterrupted;
-    // assign isBranch2 = isBranch;
-    // assign PCWrite2 = PCWrite;
-    // assign lorD2 = lorD;
-    // assign MemWrite2 = MemWrite;
-    // assign MemtoReg2 = MemtoReg;
-    // assign IRWrite2 = IRWrite;
-
-    // always @* begin
-    //     INA2 = INA;
-    // end
-    // DataPath dataPath(aluControl2, ALUSrcB2, PCSource2, ALUSrcA2, RegWrite2, RegDst2,
-    // isInterrupted2, clk, isBranch, PCWrite2, lorD2, MemWrite2, MemtoReg2, IRWrite2, Op, funct);
-
-    // assign INA3 = INA2;
